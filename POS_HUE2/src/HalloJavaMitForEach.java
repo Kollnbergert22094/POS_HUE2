36e960f5d32c883120419c6c6e2287801016dd45
kollnbergert22094 <kollnbergert22094@sus.htl-grieskirchen.at>
2024-09-24 11:28:32 +0200

    did for-each-loop and forEach()-method

--- POS_HUE2/src/HalloJavaMitForEach.java
+++ POS_HUE2/src/HalloJavaMitForEach.java
@@ -1,11 +1,19 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class HalloJavaMitForEach {
 
-    List<String> stringList;
+    static List<String> stringList;
 
-    public void forEachLoop() {
-        
+    public static void main(String[] args) {
+        stringList = new ArrayList<>();
+        stringList.add("Thorsten");
+        stringList.add("Kollnberger");
+
+        for (String string : stringList) {
+            System.out.println(string);
+        }
+
+        stringList.forEach(System.out::println);
     }
-
 }
