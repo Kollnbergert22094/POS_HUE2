91628046d5f51760388b66f49c456683e3692097
kollnbergert22094 <kollnbergert22094@sus.htl-grieskirchen.at>
2024-09-30 21:16:17 +0200

    implemented the PrimeSieve Interface

---
+++ POS_HUE2/src/net/htlgkr/kollnbergert22094/PrimeSieve.java
@@ -0,0 +1,8 @@
+package net.htlgkr.kollnbergert22094;
+
+public interface PrimeSieve {
+    // Ueberprueft ob p eine Primzahl ist oder nicht.
+    boolean isPrime(int p) ;
+    // Gibt alle Primzahlen bis zu r Obergrenze auf der Konsole aus.
+    void printPrimes() ;
+}
